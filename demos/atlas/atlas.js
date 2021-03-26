ce4ebad8eb786a93b911e0b624fde50ca1fa2aa9
Nikita Matsnev <niki-sim@mail.ru>
2021-03-26 16:06:58 +0300

    delete comments

--- demos/atlas/atlas.js
+++ demos/atlas/atlas.js
@@ -4,15 +4,6 @@
     canvas = $(canvas).get(0)
     var ctx = canvas.getContext("2d")
     var particleSystem = null
-    
-    // var palette = {
-    //   "Africa": "#D68300",
-    //   "Asia": "#4D7A00",
-    //   "Europe": "#6D87CF",
-    //   "North America": "#D4E200",
-    //   "Oceania": "#4F2170",
-    //   "South America": "#CD2900"
-    // }
 
     var that = {
       init:function(system){
@@ -46,14 +37,8 @@
 
             if (!isNaN(weight)) ctx.lineWidth = weight
             
-            // if (edge.source.data.region==edge.target.data.region){
-            //   ctx.strokeStyle = palette[edge.source.data.region] // цвет ребер
-            //   // ctx.strokeStyle = "red"
-            // }
-            
             // if (color) ctx.strokeStyle = color
             ctx.fillStyle = null // хз
-            // ctx.fillStyle = "#FF0000"
             
             ctx.moveTo(pt1.x, pt1.y)
             ctx.lineTo(pt2.x, pt2.y)
@@ -76,35 +61,23 @@
           // drawing a text label (awful alignment jitter otherwise...)
           var w = ctx.measureText(node.data.label||"").width + 6 // ширина текста
           var label = node.data.label
-          // console.log(w, label)
+
           if (!(label||"").match(/^[ \t]*$/)){
             pt.x = Math.floor(pt.x)
             pt.y = Math.floor(pt.y)
           }else{
             label = null
           }
-          
-          // clear any edges below the text label
-          // ctx.fillStyle = 'rgba(255,255,255,.6)'
-          // ctx.fillRect(pt.x-w/2, pt.y-7, w,14)
-
 
           ctx.clearRect(pt.x-w/2, pt.y-7, w,14) // очистить область
           ctx.fillStyle = 'rgba(255,88,88,.6)'// цвет области
           ctx.fillRect(pt.x-w/2, pt.y-7, w,14) // нарисовать область
 
-          
-
           // draw the text
           if (label){
             ctx.font = "bold 11px Arial"
             ctx.textAlign = "center"
-            
-            // if (node.data.region) ctx.fillStyle = palette[node.data.region]
-            // else ctx.fillStyle = "#888888"
             ctx.fillStyle = "#888888" // цвет текста
-
-            // ctx.fillText(label||"", pt.x, pt.y+4)
             ctx.fillText(label||"", pt.x, pt.y+4)
           }
         })    		
@@ -120,51 +93,49 @@
 
     	initMouseHandling:function(){
         // no-nonsense drag and drop (thanks springy.js)
-      	selected = null;
+      	// selected = null;
       	nearest = null;
       	var dragged = null;
         var oldmass = 1
 
         $(canvas).mousedown(function(e){
-          //window.open("https://google.com");
       		var pos = $(this).offset();
       		var p = {x:e.pageX-pos.left, y:e.pageY-pos.top}
-      		selected = nearest = dragged = particleSystem.nearest(p);
-          // console.log(pos, p, nearest)
-          
-      		if (selected.node !== null && nearest.distance < 30){
+      		// selected = 
+          if (particleSystem.nearest(p).distance < 30 && e.shiftKey) {
+            nearest = dragged = particleSystem.nearest(p);
+          }
+
+      		if (dragged && dragged.node !== null && nearest.distance < 10){
             // window.location.href = "https://google.com/" + dragged.node.data.label;
-            // dragged.node.tempMass = 10000
-            // console.log(dragged.node.data.label)
             dragged.node.fixed = true
       		}
       		return false
       	});
 
-      	// $(canvas).mousemove(function(e){
-        //   var old_nearest = nearest && nearest.node._id
-      	// 	var pos = $(this).offset();
-      	// 	var s = {x:e.pageX-pos.left, y:e.pageY-pos.top};
+      	$(canvas).mousemove(function(e){
+          var old_nearest = nearest && nearest.node._id
+      		var pos = $(this).offset();
+      		var s = {x:e.pageX-pos.left, y:e.pageY-pos.top};
 
-      	// 	nearest = particleSystem.nearest(s);
-        //   if (!nearest) return
+      		nearest = particleSystem.nearest(s);
+          if (!nearest) return
 
-      	// 	if (dragged !== null && dragged.node !== null){
-        //     var p = particleSystem.fromScreen(s)
-      	// 		dragged.node.p = {x:p.x, y:p.y}
-        //     // dragged.tempMass = 10000
-      	// 	}
+      		if (dragged !== null && dragged.node !== null){
+            var p = particleSystem.fromScreen(s)
+      			dragged.node.p = {x:p.x, y:p.y}
+            // dragged.tempMass = 10000
+      		}
 
-        //   return false
-      	// });
+          return false
+      	});
 
       	$(window).bind('mouseup',function(e){
           if (dragged===null || dragged.node===undefined) return
-          // console.log("up", dragged, dragged.node, selected)
           dragged.node.fixed = false
           dragged.node.tempMass = 100
       		dragged = null;
-      		selected = null
+      		// selected = null
       		return false
       	});
       	      
@@ -175,72 +146,7 @@
     return that
   }
   
-  // var Maps = function(elt){
-  //   var sys = arbor.ParticleSystem(4000, 500, 0.5, 55)
-  //   sys.renderer = Renderer("#viewport") // our newly created renderer will have its .init() method called shortly by sys...
-    
-  //   var dom = $(elt)    
-  //   var _links = dom.find('ul')
-
-  //   var _sources = {
-  //     nations:'Derived from Wikipedia’s <a target="_blank" href="http://en.wikipedia.org/wiki/List_of_countries_and_territories_by_land_borders">List of countries and territories by land borders</a>',
-  //     states:'Derived from <a target="_blank" href="http://www.statemaster.com/graph/geo_lan_bou_bor_cou-geography-land-borders">Land borders by state</a>',
-  //     risk:'Derived from Garrett Robinson’s <a target="_blank" href="http://web.mit.edu/sp.268/www/risk.pdf">The Strategy of Risk</a>'
-  //   }
-
-  //   var _maps = {
-  //     usofa:{title:"United States", p:{stiffness:600}, source:_sources.states},
-  //     africa:{title:"Africa", p:{stiffness:300}, source:_sources.nations},
-  //     asia:{title:"Asia", p:{stiffness:500}, source:_sources.nations},
-  //     europe:{title:"Europe", p:{stiffness:300}, source:_sources.nations},
-  //     mideast:{title:"Middle East", p:{stiffness:500}, source:_sources.nations},
-  //     risk:{title:"Risk", p:{stiffness:400}, source:_sources.risk}
-  //   }
-    
-  //   var that = {
-  //     init:function(){
-        
-  //       $.each(_maps, function(stub, map){
-  //         _links.append("<li><a href='#/"+stub+"' class='"+stub+"'>"+map.title+"</a></li>")
-  //       })
-  //       _links.find('li > a').click(that.mapClick)
-  //       _links.find('.usofa').click()
-  //       return that
-  //     },
-  //     mapClick:function(e){
-  //       var selected = $(e.target)
-  //       var newMap = selected.attr('class')
-  //       if (newMap in _maps) that.selectMap(newMap)
-  //       _links.find('li > a').removeClass('active')
-  //       selected.addClass('active')
-  //       return false
-  //     },
-  //     selectMap:function(map_id){
-  //       $.getJSON("maps/"+map_id+".json",function(data){
-  //         // load the raw data into the particle system as is (since it's already formatted correctly for .merge)
-  //         var nodes = data.nodes
-  //         $.each(nodes, function(name, info){
-  //           info.label=name.replace(/(people's )?republic of /i,'').replace(/ and /g,' & ')
-  //         })
-
-  //         sys.merge({nodes:nodes, edges:data.edges})
-  //         sys.parameters(_maps[map_id].p)
-  //         $("#dataset").html(_maps[map_id].source)
-  //       })
-        
-  //     }
-  //   }
-    
-  //   return that.init()    
-  // }
-  
-  
-  
-  
   $(document).ready(function(){
-
-    // var mcp = Maps("#maps")
-
 
     var sys = arbor.ParticleSystem(4000, 500, 0.5, 55)
     sys.renderer = Renderer("#viewport") 
@@ -248,23 +154,15 @@
     $.getJSON("maps/risk.json", function(data){
       // load the raw data into the particle system as is (since it's already formatted correctly for .merge)
       var nodes = data.nodes
-      console.log(data)
+      // console.log(data)
       $.each(nodes, function(name, info){
         info.label=name.replace(/(people's )?republic of /i,'').replace(/ and /g,' & ')
       })
 
       sys.merge({nodes:nodes, edges:data.edges})
       sys.parameters({stiffness:400})
-      // $("#dataset").html(_maps[map_id].source)
     })
 
   })
   
-  
-  
-  
-  
-  
-  
-	
 })()
